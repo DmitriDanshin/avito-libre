d79c2709edd7d24b945465fa32ebd4b46a76bc38
Kostya <serafi97@mail.ru>
2022-07-29 09:29:58 +0300

    test date on value error for any single case

--- src/tests/test_date.py
+++ src/tests/test_date.py
@@ -19,13 +19,14 @@ class TestDate(TestCase):
     def test_date_incorrect(self):
         with self.assertRaises(ValueError):
             DateHandler.reformat_date('2018 апрель 25 11:15')
-            # todo вынести в отдельный assertRaises
-            # DateHandler.reformat_date('14.11.2005 16:48')
-            #
-            # DateHandler.reformat_date('27 июль 11:15')
-            # DateHandler.reformat_date('26.09.2001')
-            # DateHandler.reformat_date('12 12')
-
+        with self.assertRaises(ValueError):
+            DateHandler.reformat_date('14.11.2005 16:48')
+        with self.assertRaises(ValueError):
+            DateHandler.reformat_date('27 июль 11:15')
+        with self.assertRaises(ValueError):
+            DateHandler.reformat_date('26.09.2001')
+        with self.assertRaises(ValueError):
+            DateHandler.reformat_date('12 12')
         with self.assertRaises(ValueError):
             DateHandler.reformat_date('27 июля 25 11:15')
 
